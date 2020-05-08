20e765f2f198429cbc5cc412eb38aebb805fca89
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-08 08:54:04 -0600

    Determine if a string is an isogram

--- isograms.js
+++ isograms.js
@@ -2,12 +2,16 @@
 // Implement a function that determines whether a string that contains only letters is an isogram. 
 // Assume the empty string is an isogram. Ignore letter case.
 
-function isIsogram(str){
-  // turn the string to lowercase
-  // iterate over str and check if there are repeating chars
-  // if repeating char return false
-  // else true
+function isIsogram(str) {
+  str = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+      let slice = str.slice(i+1, str.length);
 
+      if (slice.indexOf(str[i]) !== -1) {
+          return false
+      } 
+  }
+  return true;
 }
 
 isIsogram("Dermatoglyphics") == true
