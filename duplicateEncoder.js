880a5a69d22b291fcdbd2afeae12bd0a475753b5
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-12 08:40:22 -0600

    Encodes most words. Bug when only 1 repeating char

--- duplicateEncoder.js
+++ duplicateEncoder.js
@@ -5,17 +5,31 @@
 // is a duplicate.
 
 
-const duplicateEncoder = (word) => {
-    // toLowerCase
-    // iterate over string
-        // splice string to remove the current char
-        // if that char is still in the word
-            // push ) to return str
-        // else
-            // push (
+const duplicateEncode = (word) => {
+    let returnStr = '';
+    word = word.toLowerCase().split('').map(char => {
+        let count = 0;
+        let idx = 0;
+        while (idx !== -1) {
+            idx = word.indexOf(char, idx+1)
+            console.log('idx=',idx);
+            if (idx !== -1){
+                count++;
+            }
+            console.log(count);
+            if (count >= 2) {
+                break;
+            }
+        }
+        count > 1 ? returnStr += ')' : returnStr += '(';
+        console.log(returnStr);
+    })
+
+    console.log(returnStr);
+    return returnStr;
 }
 
-duplicateEncode("din"); // ==== (((
-duplicateEncode("recede"); // === ()()()
-duplicateEncode("Success"); // === )())())
+// duplicateEncode("Din"); // ==== (((
+// duplicateEncode("recede"); // === ()()()
+// duplicateEncode("Success"); // === )())())
 duplicateEncode("(( @"); // ==== ))((
