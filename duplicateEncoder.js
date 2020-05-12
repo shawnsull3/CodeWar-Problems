d180e9283bdb2c085821bb3dc7fe535373c2c286
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-12 08:51:13 -0600

    Fix skipping counts due to idx increase

--- duplicateEncoder.js
+++ duplicateEncoder.js
@@ -8,28 +8,24 @@
 const duplicateEncode = (word) => {
     let returnStr = '';
     word = word.toLowerCase().split('').map(char => {
-        let count = 0;
-        let idx = 0;
+        let count = idx = 0;
         while (idx !== -1) {
-            idx = word.indexOf(char, idx+1)
-            console.log('idx=',idx);
+            idx = word.indexOf(char, idx);
             if (idx !== -1){
                 count++;
+                idx++;
             }
-            console.log(count);
             if (count >= 2) {
                 break;
             }
         }
         count > 1 ? returnStr += ')' : returnStr += '(';
-        console.log(returnStr);
     })
-
-    console.log(returnStr);
     return returnStr;
 }
 
-// duplicateEncode("Din"); // ==== (((
-// duplicateEncode("recede"); // === ()()()
-// duplicateEncode("Success"); // === )())())
+duplicateEncode("Din"); // ==== (((
+duplicateEncode("recede"); // === ()()()
+duplicateEncode("Success"); // === )())())
 duplicateEncode("(( @"); // ==== ))((
+duplicateEncode("bbac"); // === ())(
