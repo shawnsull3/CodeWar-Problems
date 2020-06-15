eeb69bc3aea38393050ada945f90b118f6ecbe8b
Shawn Sullivan <sullivansm3@gmail.com>
2020-06-15 15:49:49 -0600

    Parse out the doamin name of a URL

---
+++ domainName.js
@@ -0,0 +1,15 @@
+// Write a function that when given a URL as a string, parses 
+// out just the domain name and returns it as a string.
+
+function domainName(url){
+    url = url.replace("https://", '');
+    url = url.replace("http://", '');
+    url = url.replace("www.", '');
+
+    console.log(url.split('.')[0])
+    return url.split('.')[0];
+}
+
+domainName("http://github.com/carbonfive/raygun") // "github" 
+domainName("http://www.zombie-bites.com") // "zombie-bites"
+domainName("https://www.cnet.com") // "cnet"
