028c17cdffb2a2b583720b633b5599724276859f
Shawn Sullivan <sullivansm3@gmail.com>
2020-06-17 17:38:27 -0600

    Check sudoku solution rows & columns

---
+++ sudokuValidator.js
@@ -0,0 +1,48 @@
+function validSolution(board){
+    // row check
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            if (board[row].lastIndexOf(board[row][col]) !== col) {
+                return false;
+            }
+        }
+    }
+    // col check
+    for (let col = 0; col < 9; col++) {
+        let array = []
+        for (let row = 0; row < 9; row++) {
+            array.push(board[row][col])
+        }
+        for (let i = 0; i < 9; i++) {
+            if (array.lastIndexOf(array[i]) !== i) {
+                return false;
+            }
+        }
+    }
+
+    // square check
+}
+
+validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]); // => true
+
+validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2], 
+    [6, 7, 2, 1, 9, 0, 3, 4, 8],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9]
+]); // => false
