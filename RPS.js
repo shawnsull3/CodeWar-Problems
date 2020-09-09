d77cbe501195e852507a8d475207b140ab75ee8d
Shawn Sullivan <sullivansm3@gmail.com>
2020-09-09 08:39:56 -0600

    Setup problem

---
+++ RPS.js
@@ -0,0 +1,26 @@
+/*
+* Write a function that generates every sequence of throws a single
+* player could throw over a three-round game of rock-paper-scissors.
+*
+* Your output should look something like:
+*   ["RRR",
+*    "RRP",
+*    "RRS",
+*    "RPR",
+*    ...etc...
+*   ]
+*
+* Example:
+* rockPaperScissors(5); // => ['RRRRR', 'RRRRP', 'RRRRS', etc...]
+*/
+
+var rockPaperScissors = function (n) {
+    let results = [];
+
+    // recursive solution will be needed
+
+    return results;
+};
+  
+// rockPaperScissors(2);
+rockPaperScissors(3);
