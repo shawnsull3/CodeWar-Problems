ec0606dd9065e11ac4d4b7c53c17b04f6d18af8e
Shawn Sullivan <sullivansm3@gmail.com>
2020-09-22 08:42:18 -0600

    Setup problem & pseudocode approach

---
+++ permutations.js
@@ -0,0 +1,24 @@
+// Given a collection of distinct integers, return all possible permutations.
+
+// Example:
+
+// Input: [1,2,3]
+// Output:
+// [
+//   [1,2,3],
+//   [1,3,2],
+//   [2,1,3],
+//   [2,3,1],
+//   [3,1,2],
+//   [3,2,1]
+// ]
+
+var permute = function(nums) {
+    let permutations = [];
+
+    // create a recursive function that adds each of the elements to an array
+    // once array len === nums len push
+        // push to results and return
+
+    return permutations
+};
